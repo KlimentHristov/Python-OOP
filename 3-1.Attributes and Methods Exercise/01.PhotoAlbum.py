5beb5bb36e79254dc713417a5fb2cc751f5ab1fd
Kliment Hristov <kliment.hristov@gmail.com>
2021-02-24 21:24:03 +0200

    More !important exercise with attributes

---
+++ 3-1.Attributes and Methods Exercise/01.PhotoAlbum.py
@@ -0,0 +1,31 @@
+from math import ceil
+class PhotoAlbum:
+    def __init__(self, pages: int):
+        self.pages = pages
+        self.photos = [[] for i in range(pages)]
+
+    @classmethod
+    def from_photos_count(cls, photos_count: int):
+        return cls(ceil(photos_count/4))
+
+
+    def add_photo(self, label: str):
+        for i in range(len(self.photos)):
+            if len(self.photos[i]) < 4:
+                self.photos[i].append(label)
+                return f"{label} photo added successfully on page {i+1} slot {len(self.photos[i])}"
+        return "No more free spots"
+
+
+    def display(self):
+        result = ""
+        for i in range(len(self.photos)):
+            result += "-"*11+"\n"
+            if len(self.photos[i]) == 0:
+                result+= "\n"
+            else:
+                result+= "[] "*len(self.photos[i])
+                result = result.rstrip()
+                result+= "\n"
+        result += "-"*11+"\n"
+        return result
